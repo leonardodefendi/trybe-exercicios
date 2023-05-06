51b7ca8fbd6e74b47a87fefd5bc6ad352cf556b0
leonardodefendi <leo_defendi@hotmail.com>
2023-05-06 14:00:52 -0300

    Exercicios de 1 a 5 finalizados 3.4

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios-fundamentos-3.4/scriptex1-5.js
@@ -0,0 +1,45 @@
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// // console.log("Bem-vinda,", info.personagem ) //requisito 1
+
+
+// info["recorrente"] = "Sim";
+// // console.log(info)// requisito 2
+
+// // for(let keys in info){
+// //   console.log(keys);
+// // } // requisito 3
+
+
+// // for(let key in info){
+// //   console.log(info[key])
+  
+
+// // } //requisito 4
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #17',
+//   nota: 'O último MacPatinhas',
+//   recorrente: "Sim"
+// };
+
+// let valuesInfo = Object.values(info);
+// let valuesInfo2 = Object.values(info2);
+
+// if(valuesInfo.length == valuesInfo2.length){
+//   for (let index in valuesInfo){
+//     if(valuesInfo[index] === valuesInfo2[index]){
+//       console.log("Ambos recorrentes")
+//     }
+//     else{
+//       console.log(valuesInfo[index], valuesInfo2[index])
+//     }
+    
+
+
+//   }
+// }// requisito 5
