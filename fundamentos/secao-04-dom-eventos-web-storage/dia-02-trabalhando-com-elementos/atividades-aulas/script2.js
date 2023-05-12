f0600571985dafd28463315052992acc6ec8db11
leonardodefendi <leo_defendi@hotmail.com>
2023-05-12 15:08:48 -0300

    Finalizando atividades

--- fundamentos/secao-04-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/atividades-aulas/script2.js
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/atividades-aulas/script2.js
@@ -1,21 +1,24 @@
 // Crie um irmão para elementoOndeVoceEsta.
-const newElement = document.createElement ('section');
-newElement.innerText = 'Hello, word!';
-let pai = document.querySelector('#pai');
-pai.appendChild(newElement);
+// const newElement = document.createElement ('section');
+// newElement.innerText = 'Hello, word!';
+// let pai = document.querySelector('#pai');
+// pai.appendChild(newElement);
 
-// Crie um filho para elementoOndeVoceEsta.
-const newElement2 = document.createElement('section');
-newElement2.innerHTML= '<strong>Agora estou entendendo</strong>';
-let pai2 = document.querySelector('#elementoOndeVoceEsta');
-pai2.appendChild(newElement2);
+// // Crie um filho para elementoOndeVoceEsta.
+// const newElement2 = document.createElement('section');
+// newElement2.innerHTML= '<strong>Agora estou entendendo</strong>';
+// let pai2 = document.querySelector('#elementoOndeVoceEsta');
+// pai2.appendChild(newElement2);
 
-// Crie um filho para primeiroFilhoDoFilho.
-const newElement3 = document.createElement('li');
-newElement3.id = 'criado'
-newElement3.innerText ='Finalizando Exercicio';
-let pai3 = document.querySelector('#primeiroFilhoDoFilho');
-pai3.appendChild(newElement3);
+// // Crie um filho para primeiroFilhoDoFilho.
+// const newElement3 = document.createElement('li');
+// newElement3.id = 'criado'
+// newElement3.innerText ='Finalizando Exercicio';
+// let pai3 = document.querySelector('#primeiroFilhoDoFilho');
+// pai3.appendChild(newElement3);
 
-// A partir desse filho criado, acesse terceiroFilho.
-console.log(document.querySelector('#criado').parentNode.parentNode.nextElementSibling);
+// // A partir desse filho criado, acesse terceiroFilho.
+// console.log(document.querySelector('#criado').parentNode.parentNode.nextElementSibling);
+
+const element = document.querySelector('#where-are-you').firstElementChild
+document.querySelector('#where-are-you').removeChild(element);
