fe4f01a96503a56d672d6ad190400e0a6dc16469
leonardodefendi <leo_defendi@hotmail.com>
2023-05-17 08:20:07 -0300

    Finalizando requisito 4

--- fundamentos/secao-04-dom-eventos-web-storage/dia-04-web-storage/exercicios-dia4.4/script.js
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-04-web-storage/exercicios-dia4.4/script.js
@@ -6,25 +6,40 @@ const trocarFundo = () =>{
     body.style.backgroundColor = event.target.innerText;
   })
   }
-}                           
+};                           
 
 
-const colorText = document.querySelectorAll('.btncolortext')
+const colorText = document.querySelectorAll('.btncolortext');
 const changeColorText = () => {
   for(index = 0; index < colorText.length; index += 1){
     colorText[index].addEventListener('click', (event) =>{
       body.style.color = event.target.innerText;
     })
   }
-}
+};
 
-const textSize = document.querySelectorAll('#font-size > button')
+const textSize = document.querySelectorAll('#font-size > button');
+const changeTextSize = () =>{
+  for(index = 0; index < textSize.length; index += 1){
+    textSize[index].addEventListener('click', (event) =>{
+      body.style.fontSize = event.target.innerText;
+    })
+  }
+};
+
+const lineHeight = document.querySelector ('#line-height')
+const lineHeightChild = lineHeight.children;
+const changeLineHeight = () =>{
+  for(index = 1; index < lineHeightChild.length; index += 1){
+    lineHeightChild[index].addEventListener('click', (event) =>{
+      body.style.lineHeight = event.target.innerText;
+    })
+  }
+};
 
 
-
-
-
+changeLineHeight();
 changeColorText();
 trocarFundo();
-
+changeTextSize();
 
