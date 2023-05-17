00704ec6deb573e4a8aed3c6d7d57e1530a51762
leonardodefendi <leo_defendi@hotmail.com>
2023-05-17 09:22:19 -0300

    Finalizando todos exercicios

--- fundamentos/secao-04-dom-eventos-web-storage/dia-04-web-storage/exercicios-dia4.4/script.js
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-04-web-storage/exercicios-dia4.4/script.js
@@ -3,7 +3,9 @@ const body = document.body;
 const trocarFundo = () =>{
   for(let index = 0; index < backgroundColor.length; index += 1){
   backgroundColor[index].addEventListener('click', (event) =>{
-    body.style.backgroundColor = event.target.innerText;
+    const colorbackground = event.target.innerText;
+    body.style.backgroundColor = colorbackground
+    localStorage.setItem('background', colorbackground);
   })
   }
 };                           
@@ -14,6 +16,7 @@ const changeColorText = () => {
   for(index = 0; index < colorText.length; index += 1){
     colorText[index].addEventListener('click', (event) =>{
       body.style.color = event.target.innerText;
+      localStorage.setItem('colortext', event.target.innerText)
     })
   }
 };
@@ -23,6 +26,7 @@ const changeTextSize = () =>{
   for(index = 0; index < textSize.length; index += 1){
     textSize[index].addEventListener('click', (event) =>{
       body.style.fontSize = event.target.innerText;
+      localStorage.setItem('textsize', event.target.innerText)
     })
   }
 };
@@ -33,13 +37,43 @@ const changeLineHeight = () =>{
   for(index = 1; index < lineHeightChild.length; index += 1){
     lineHeightChild[index].addEventListener('click', (event) =>{
       body.style.lineHeight = event.target.innerText;
+      localStorage.setItem('lineheight', event.target.innerText)
     })
   }
 };
 
+const fontFamily = document.querySelectorAll('#font-family > button');
+const changeFontFamily = () => {
+  for(index = 0; index < fontFamily.length; index += 1){
+    fontFamily[index].addEventListener('click', (event) =>{
+      body.style.fontFamily = event.target.innerText;
+      localStorage.setItem('fontfamily', event.target.innerText);
+    })
+  }
+};
 
+const localStorageRecovery = () =>{
+  const restore = localStorage.getItem('background')
+  body.style.backgroundColor = restore
+
+  const restoreColorText = localStorage.getItem('colortext');
+  body.style.color = restoreColorText;
+
+  const restoreTextSize = localStorage.getItem('textsize');
+  body.style.fontSize = restoreTextSize;
+
+  const restoreLineHeight = localStorage.getItem('lineheight');
+  body.style.lineHeight = restoreLineHeight;
+
+  const restoreFontFamily = localStorage.getItem('fontfamily');
+  body.style.fontFamily = restoreFontFamily;
+}
+
+window.onload = () =>{
+changeFontFamily();
 changeLineHeight();
 changeColorText();
 trocarFundo();
 changeTextSize();
-
+localStorageRecovery();
+}
