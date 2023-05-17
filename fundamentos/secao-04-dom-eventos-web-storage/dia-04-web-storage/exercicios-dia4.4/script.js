fef2cae2986918d53dd7f7bb8773d79eec5941d4
leonardodefendi <leo_defendi@hotmail.com>
2023-05-17 08:03:19 -0300

    Finalizando requisito 2

--- fundamentos/secao-04-dom-eventos-web-storage/dia-04-web-storage/exercicios-dia4.4/script.js
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-04-web-storage/exercicios-dia4.4/script.js
@@ -7,8 +7,24 @@ const trocarFundo = () =>{
   })
   }
 }                           
+
+
+const colorText = document.querySelectorAll('.btncolortext')
+const changeColorText = () => {
+  for(index = 0; index < colorText.length; index += 1){
+    colorText[index].addEventListener('click', (event) =>{
+      body.style.color = event.target.innerText;
+    })
+  }
+}
+
+const textSize = document.querySelectorAll('#font-size > button')
+
+
+
+
+
+changeColorText();
 trocarFundo();
-
-
 
 
