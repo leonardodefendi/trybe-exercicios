2d3cad019ebb7941c22a7503aa50d8a08eefc731
leonardodefendi <leo_defendi@hotmail.com>
2023-05-19 20:11:31 -0300

    Finalizando as atividades

---
+++ fundamentos/secao-05-htm-css-forms-flexbox-reponsivo/dia-01-htm-css-forms/atividades-aula/script.js
@@ -0,0 +1,20 @@
+// SELETORES
+const inputText = document.querySelector("#input-text");
+const inputCheckbox = document.querySelector("#input-checkbox");
+const hrefLink = document.querySelector("#href");
+
+
+
+hrefLink.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+inputCheckbox.addEventListener('click', (event)=>{
+  event.preventDefault();
+})
+inputText.addEventListener('keypress', (event) =>{
+  const teste = event.key;
+  if (teste !== 'a'){
+    event.preventDefault();
+  }
+})
