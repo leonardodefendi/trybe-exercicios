cb5cdb7496c27f4683a9fe21115887eaef38c3ac
leonardodefendi <leo_defendi@hotmail.com>
2023-05-30 09:45:52 -0300

    Refatorando parteIII

--- fundamentos/secao-06-intro-es6-testes-unitarios/dia-01-fluxo-excecoes/exercicios-aula-6.1/exercicio-parteIII/script.js
+++ fundamentos/secao-06-intro-es6-testes-unitarios/dia-01-fluxo-excecoes/exercicios-aula-6.1/exercicio-parteIII/script.js
@@ -1,6 +1,7 @@
 const studentRegister = (name, age) => {
 try {
-  return verifyInputsValue(name, age);
+  verifyInputsValue(name, age);
+  return `${name}, seja bem-vindo(a) á AuTrybe!`
 } catch (error) {
   return error.message;
 }
@@ -8,12 +9,12 @@ try {
 
 
 const verifyInputsValue = (name, age) => {
-  if(name === '' || age === '') {
+  if(!name || !age) {
     throw new Error ('Todas as informações são necessárias');
   } if (age < 18) {
     throw new Error ('Ops, infelizmente nesse momento você não pode fazer as aulas');
-  } if (age >= 18) {
-    return `${name}, seja bem-vindo(a) á AuTrybe!`
-  }
+  } 
 }
 
+
+console.log(studentRegister('Leonardo', 18))
