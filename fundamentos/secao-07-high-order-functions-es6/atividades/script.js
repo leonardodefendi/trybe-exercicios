dd2178136c26e529a8bb902b931ce8bec29797a1
leonardodefendi <leo_defendi@hotmail.com>
2023-06-05 09:32:30 -0300

    Iniciando seção 7

---
+++ fundamentos/secao-07-high-order-functions-es6/atividades/script.js
@@ -0,0 +1,11 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+
+emailListInData.forEach((element) => {
+  console.log(`O email ${element} está cadastrado em nosso banco de dados!`);
+})
