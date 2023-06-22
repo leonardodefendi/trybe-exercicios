bc02517f0f72cb3819337fb543f00ab396485c61
leonardodefendi <leo_defendi@hotmail.com>
2023-06-22 19:36:37 -0300

    Finaliza exercicio 1, exercicios do dia

---
+++ front-end/secao-01-introducao-frontend-js-assincrono/dia-02-javascrip-assincrono-primeses-fetech/exercicios-frontend-1.2/src/main.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+const superHeroimg = document.querySelector('#image');
+const superHeroName = document.querySelector('#name');
+const btnSortear = document.querySelector('#btn-sortear');
+const sorteadorNumero = () => Math.round(Math.random() * 1000);
+
+btnSortear.addEventListener('click', () => {
+  superHeroimg.src = '';
+  superHeroName.innerHTML = '';
+  fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${sorteadorNumero()}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      superHeroName.innerHTML = data.name;
+      superHeroimg.src = data.images.md;
+    })
+    .catch((error) => error.message);
+});
