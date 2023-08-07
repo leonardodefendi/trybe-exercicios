4f19c7567dea818f24f8c84d8d6352fd36ed4c87
leonardodefendi <leo_defendi@hotmail.com>
2023-08-07 14:20:00 -0300

    Fix

--- front-end/secao-03-aprofundando-react/dia-02-useeffect/atividade-3.2/src/App.tsx
+++ front-end/secao-03-aprofundando-react/dia-02-useeffect/atividade-3.2/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { fetchCoordinates } from './services';
 
+type CoordinatesType = {
+  latitude: number,
+  longitude: number,
+};
 function App() {
-  const [count, setCount] = useState(0)
+  const [coordinates, setCoordinates] = useState<CoordinatesType | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>International Space Station Location Tracker</h1>
+      {coordinates && (
+        <>
+          <h1>{`Latitude: ${coordinates.latitude}`}</h1>
+          <h1>{`Longitude: ${coordinates.longitude}`}</h1>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
